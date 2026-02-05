5de598ec53ce72b2cc5486b3dc4b467f58e2d88d
Rojan Aryal <roozanaryal8@gmail.com>
2026-02-05 11:28:29 +0545

    commit

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -5,7 +5,9 @@ import { validationResult } from "express-validator";
 export const registerUser = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+    return res.status(400).json({
+      errors: errors.array(),
+    });
   }
 
   const { fullname, email, password } = req.body;
@@ -19,4 +21,28 @@ export const registerUser = async (req, res, next) => {
   });
   const token = user.generateAuthToken();
   res.status(201).json({ user, token });
+};
+
+export const loginUser = async (req, res, next) => {
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    return res.status(400).json({
+      error: error.array(),
+    });
+
+  }
+  const {email,password}=req.body;
+
+  const user = await userModel.findOne({email}).select('+password')
+  if(!user){
+    return res.status(401).json({message:"Invalid email or password"})
+  }
+  const isMatched = await bcrypt.compare(password, user.password)
+  if(!isMatched){
+    return res.status(401).json({message:"Invalid email or password"})
+
+  }
+
+  const token = await user.generateAuthToken()
+  
 };
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,16 +1,29 @@
 import express from "express";
-import { registerUser } from "../controllers/auth.controller.js";
+import { registerUser,loginUser } from "../controllers/auth.controller.js";
 import { body } from "express-validator";
 // import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/register", [
-    body('email').isEmail().withMessage('Invalid Email'),
-    body('fullname.firstName').isLength({ min: 3 }).withMessage("FirstName must be min of 3 character"),
-    body('password').isLength({ min: 6 }).withMessage("write a long password")
+router.post(
+  "/register",
+  [
+    body("email").isEmail().withMessage("Invalid Email"),
+    body("fullname.firstName")
+      .isLength({ min: 3 })
+      .withMessage("FirstName must be min of 3 character"),
+    body("password").isLength({ min: 6 }).withMessage("write a long password"),
+  ],
+  registerUser,
+);
 
-], registerUser);
-
+router.post(
+  "/login",
+  [
+    body("email").isEmail().withMessage("Invalid Email format"),
+    body("password").minLength({ min: 6 }).withMessage("write a long password"),
+  ],
+  loginUser,
+);
 
 export default router;
